0b8a0724b09070e55cc429c9911c783a0c2067e5
zkstud <100755286+zkstud@users.noreply.github.com>
2022-03-18 10:59:26 +0100

    dodalem przycisk oraz dwa pola textField

--- src/main/java/com/sample/lab2/Controller.java
+++ src/main/java/com/sample/lab2/Controller.java
@@ -1,8 +1,21 @@
 package com.sample.lab2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    public Button btnMain;
+    int clickCounter = 0;
+    public TextField txtLicznik, txtLicznikRe;
 
+    public void onBtnAction(ActionEvent actionEvent) {
+        txtLicznikRe.setText(" ");
+        txtLicznik.setText("Klikniecie nr: " + ++clickCounter);
+        if (clickCounter == 1 || clickCounter == 5) {
+            txtLicznikRe.setText("I jeszcze raz!");
+        }
+    }
 }
